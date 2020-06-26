bca33483369b0739707cf33d940509f38643a144
Ben Adida <ben@adida.net>
2020-06-26 14:02:16 -0700

    autoconfig printer

--- run-kiosk-browser.sh
+++ run-kiosk-browser.sh
@@ -1,2 +1,2 @@
 
-kiosk-browser --allowed-save-as-hostname-pattern localhost --allowed-save-as-destination-pattern "/media/**/*" http://localhost:3000 --allow-devtools
+kiosk-browser --allowed-save-as-hostname-pattern localhost --allowed-save-as-destination-pattern "/media/**/*" --autoconfigure-print-config config/printer-autoconfigure.json --url http://localhost:3000 --allow-devtools
