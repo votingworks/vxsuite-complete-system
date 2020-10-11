84e4ffcc670aa6c57014da38abbc3016064183aa
Ben Adida <ben@adida.net>
2020-10-10 18:10:42 -0700

    add environment variables and config stuff that didn't get ported over.

--- run-election-manager.sh
+++ run-election-manager.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 # go to directory where this file is located
 cd "$(dirname "$0")"
 
+: "${VX_CONFIG_ROOT:="./config"}"
+
 # configuration information
-source config/read-vx-machine-config.sh
+source ${VX_CONFIG_ROOT}/read-vx-machine-config.sh
 
 export PIPENV_VENV_IN_PROJECT=1
 (trap 'kill 0' SIGINT SIGHUP; make -C frontends/election-manager run)
