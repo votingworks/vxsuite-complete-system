d9a20d002021ced31f9d027d0b00301978014516
Caroline Modic <caroline@voting.works>
2020-11-17 11:08:29 -0800

    fix removing ubuntu desktop

--- setup-machine.sh
+++ setup-machine.sh
@@ -211,7 +211,7 @@ echo "Successfully setup machine."
 USER=$(whoami)
 
 # remove all unnecessary packages
-sudo apt remove -y --auto-remove ubuntu-gnome-desktop
+sudo apt remove -y ubuntu-desktop
 sudo apt remove -y git firefox snapd
 sudo apt autoremove -y
 
