d7216f4d8ece336988d0f18e442af696e9359580
Caroline Modic <caroline@voting.works>
2021-01-11 13:38:53 -0800

    update kiosk browser file permissions to new format

--- run-kiosk-browser.sh
+++ run-kiosk-browser.sh
@@ -2,5 +2,5 @@
 
 URL=$1
 
-kiosk-browser --allowed-save-as-hostname-pattern localhost --allowed-save-as-destination-pattern "/media/**/*" --autoconfigure-print-config ./printing/printer-autoconfigure.json --url ${URL:-http://localhost:3000}
+kiosk-browser --add-file-perm o=http://localhost:*,p=/media/**/*,rw --autoconfigure-print-config ./printing/printer-autoconfigure.json --url ${URL:-http://localhost:3000}
 
