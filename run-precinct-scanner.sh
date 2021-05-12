c1c2c27a50e3d023bd5f7236a965d473831a17c8
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-05-12 14:03:29 -0700

    feat(scan): add `precinct-scanner` app
    This pins vxsuite to the latest draft version that includes the `precinct-scanner` prototype.

---
+++ run-precinct-scanner.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# go to directory where this file is located
+cd "$(dirname "$0")"
+
+# configuration information
+CONFIG=${VX_CONFIG_ROOT:-./config}
+source ${CONFIG}/read-vx-machine-config.sh
+
+export PIPENV_VENV_IN_PROJECT=1
+export NODE_ENV=production
+(trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/apps/module-scan run & make -C vxsuite/apps/precinct-scanner run)
