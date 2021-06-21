2255e94295ae52e4896d9771df273450bf611967
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-06-21 09:44:07 -0700

    fix: enforce the existence of config files
    Running without these doesn't make sense, but was sometimes convenient for manual testing. This commit also adds the files to `.gitignore` to make it easier to create them and use `config` as the `VX_CONFIG_ROOT` value.

--- config/read-vx-machine-config.sh
+++ config/read-vx-machine-config.sh
@@ -2,6 +2,26 @@
 
 : "${VX_CONFIG_ROOT:="/vx/config"}"
 
+if [ ! -f "${VX_CONFIG_ROOT}/machine-id" ]; then
+  echo "expected 'machine-id' file in ${VX_CONFIG_ROOT} but it could not be found" >&2
+  exit 1
+fi
+
+if [ ! -f "${VX_CONFIG_ROOT}/machine-type" ]; then
+  echo "expected 'machine-type' file in ${VX_CONFIG_ROOT} but it could not be found" >&2
+  exit 1
+fi
+
+if [ ! -f "${VX_CONFIG_ROOT}/code-version" ]; then
+  echo "expected 'code-version' file in ${VX_CONFIG_ROOT} but it could not be found" >&2
+  exit 1
+fi
+
+if [ ! -f "${VX_CONFIG_ROOT}/code-tag" ]; then
+  echo "expected 'code-tag' file in ${VX_CONFIG_ROOT} but it could not be found" >&2
+  exit 1
+fi
+
 export VX_MACHINE_ID="$(< "${VX_CONFIG_ROOT}/machine-id")"
 export VX_MACHINE_TYPE="$(< "${VX_CONFIG_ROOT}/machine-type")"
 export VX_CODE_VERSION="$(< "${VX_CONFIG_ROOT}/code-version")"
