d7ff15b05bddaa3c956e7dd14ec4b8fe1a502aad
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-07-01 09:49:26 -0700

    build: improve the build & run scripts
    - Don't hardcode the names of apps and modules.
    - Allow building any arbitrary subset of apps or "all" by default.
    - Clarify the purpose of the script through docs.

--- run.sh
+++ run.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
 
+###
+# run.sh – Run apps for testing purposes.
+#
+# This script is not used for running apps in production, but for running them
+# on a device that is not yet locked down. This may be useful as a demo machine
+# or for testing new builds/features/etc.
+###
+
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+ALL_APPS=()
+
+for app in ${DIR}/vxsuite/apps/*; do
+  if [[ -d "${app}" && "${app}" != */module-* ]]; then
+    ALL_APPS+=("$(basename "${app}")")
+  fi
+done
+
 usage() {
-  echo "usage: ./run.sh (bas|bmd|bsd|election-manager|precinct-scanner)"
+  echo "usage: ./run.sh ($(IFS=\| ; echo "${ALL_APPS[*]}"))"
+  echo
+  echo "Runs a VxSuite application for testing purposes."
 }
 
 if [ $# = 0 ]; then
   usage >&2
   exit 1
 fi
-
-ALL_APPS=(bas bmd bsd election-manager precinct-scanner)
 
 APP="$1"
 if [[ " ${ALL_APPS[@]} " =~ " ${APP} " ]]; then
@@ -32,6 +48,9 @@ if [[ " ${ALL_APPS[@]} " =~ " ${APP} " ]]; then
   export DISPLAY=:0
   cd "${DIR}/build/${APP}"
   (trap 'kill 0' SIGINT SIGHUP; "./run-${APP}.sh" & ("./run-kiosk-browser.sh"; kill 0))
+elif [[ "${APP}" = -h || "${APP}" = --help ]]; then
+  usage
+  exit 0
 else
   echo "✘ unknown app: ${APP}" >&2
   usage >&2
