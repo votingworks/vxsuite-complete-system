12534292e1f336008d717ba4d2e04314523790c3
Ben Adida <ben@adida.net>
2021-09-13 22:29:51 -0400

    use submodules for tpm2 software

--- setup-tpm2-totp.sh
+++ setup-tpm2-totp.sh
@@ -1,32 +1,29 @@
 set -euo pipefail
 
+# install core dependencies
 sudo apt -y install build-essential autoconf autoconf-archive automake m4 libtool gcc pkg-config libqrencode-dev pandoc doxygen liboath-dev iproute2 plymouth libplymouth-dev libssl-dev libjson-c-dev libcurl4-openssl-dev
 
-mkdir ./tpm2-totp
-cd tpm2-totp
-
-git clone https://github.com/tpm2-software/tpm2-tss
 (
-cd tpm2-tss
-./bootstrap
-./configure
-make 
-sudo make install
+    cd tpm2-software/tpm2-tss
+    ./bootstrap
+    ./configure
+    make 
+    sudo make install
 )
 
-git clone https://github.com/tpm2-software/tpm2-totp
-cd tpm2-totp
-./bootstrap
-./configure
-make
-sudo make install
+(
+    cd tpm2-software/tpm2-totp
+    ./bootstrap
+    ./configure
+    make
+    sudo make install
+)
 
+# reindex shared objects
 sudo ldconfig
 
+# initialize the TOTP code, which will display the QR code with the secret.
 sudo tpm2-totp --pcrs=0,7 init
 
-cd ../
-
-rm -rf tpm2-tss tpm2-totp
 
 
