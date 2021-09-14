99dc0ca0c4b8a60182458b3e73685cf2950a1609
Matt Bernhard <umbernhard@gmail.com>
2021-09-14 14:39:10 -0400

    Fixing paths

--- setup-signify.sh
+++ setup-signify.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 sudo apt -y install signify-openbsd qrencode
 
 # clear out old keys
-rm -f key.pub key.sec
+rm -f /vx/config/key.pub /vx/config/key.sec
 
 # Generate the keypair
 signify-openbsd -G -n -p /vx/config/key.pub -s /vx/config/key.sec
 
 # Output the public key for enrollment into another device
-qrencode -t UTF8 -r key.pub -o -
+qrencode -t UTF8 -r /vx/config/key.pub -o -
