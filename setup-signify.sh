c412dc1e6c385d6611c319acda457c4eeb657774
Matt Bernhard <umbernhard@gmail.com>
2021-09-15 20:33:02 +0000

    make setup script just isntall

--- setup-signify.sh
+++ setup-signify.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 set -euo pipefail 
 
-if [ $# -ne 1 ]; then
-    echo "Usage: ./setup-signify [directory]"
-    exit 1
-fi
-
-public_path="${1}/key.pub"
-secret_path="${1}/key.sec"
-
-
 sudo apt -y install signify-openbsd qrencode
-
-# clear out old keys
-rm -f "${public_path}" "${secret_path}"
-
-# Generate the keypair
-signify-openbsd -G -n -p "${public_path}" -s "${secret_path}"
-# Output the public key for enrollment into another device
-cat "${public_path}" | qrencode -t UTF8  -o -
 
