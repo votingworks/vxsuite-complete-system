faf0c6927685c88fa9ae1dae648d734c475aa972
Ben Adida <ben@adida.net>
2021-09-17 22:05:09 -0400

    load signify secret key from config directory when running kiosk-browser

--- run-scripts/run-kiosk-browser.sh
+++ run-scripts/run-kiosk-browser.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 URL=${1:-http://localhost:3000}
+: "${VX_CONFIG_ROOT:="./config"}"
 
-kiosk-browser --add-file-perm o=http://localhost:3000,p=/media/**/*,rw --autoconfigure-print-config ./printing/printer-autoconfigure.json --url ${URL}
+
+kiosk-browser --add-file-perm o=http://localhost:3000,p=/media/**/*,rw --autoconfigure-print-config ./printing/printer-autoconfigure.json --url ${URL} --signify-secret-key ${VX_CONFIG_ROOT}/key.sec
 
