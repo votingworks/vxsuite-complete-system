adebf796f7e5444c1e3ae4d48a0dd12969316563
Caroline Modic <caroline@voting.works>
2021-12-02 13:38:42 -0800

    feat(vcs) update run kiosk browser with permissions to read logs

--- run-scripts/run-kiosk-browser.sh
+++ run-scripts/run-kiosk-browser.sh
@@ -6,5 +6,11 @@ URL=${1:-http://localhost:3000}
 : "${VX_CONFIG_ROOT:="./config"}"
 
 
-kiosk-browser --add-file-perm o=http://localhost:3000,p=/media/**/*,rw --autoconfigure-print-config ./printing/printer-autoconfigure.json --url ${URL} --signify-secret-key ${VX_CONFIG_ROOT}/key.sec
+kiosk-browser \
+  --add-file-perm o=http://localhost:3000,p=/media/**/*,rw \
+  --add-file-perm o=http://localhost:3000,p=/var/log,ro \
+  --add-file-perm o=http://localhost:3000,p=/var/log/*,ro \
+  --autoconfigure-print-config ./printing/printer-autoconfigure.json \
+  --url ${URL} \
+  --signify-secret-key ${VX_CONFIG_ROOT}/key.sec
 
