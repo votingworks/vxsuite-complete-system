e309a5dccac57793f73e9f13f30d9e554db2e98c
Matt Bernhard <mdb92nc@gmail.com>
2022-01-26 14:48:07 -0500

    Tweaks for sudo

--- setup-scripts/lockdown.sh
+++ setup-scripts/lockdown.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 set -euo pipefail 
 
-sudo cp config/dmverity-root.hook /etc/initramfs-tools/hooks/dmverity-root
-sudo cp config/dmverity-root.script /etc/initramfs-tools/scripts/local-premount/dmverity-root
+cp config/dmverity-root.hook /etc/initramfs-tools/hooks/dmverity-root
+cp config/dmverity-root.script /etc/initramfs-tools/scripts/local-premount/dmverity-root
 
-sudo update-initramfs -u
+update-initramfs -u
 
 # Remount / so it can't change while we're doing the veritysetup
-sudo mount -o ro,remount /
+mount -o ro,remount /
 
 # Now do the dm-verity setup
-sudo veritysetup format --debug /dev/mapper/VxMark--vg-root /dev/mapper/VxMark--vg-hashes| tee "/tmp/verity.log"
+veritysetup format --debug /dev/mapper/VxMark--vg-root /dev/mapper/VxMark--vg-hashes| tee "/tmp/verity.log"
 
 # Find the root hash and append it to our cmdline
 HASH="$(awk '/Root hash:/ { print $3 }' "/tmp/verity.log")"
@@ -17,7 +18,7 @@ echo "$(cat config/cmdline)${HASH}" > /tmp/cmdline
 
 # TODO: Make sure the output initramfs/kernel file names are correct!
 # Now package up ouer kernel, cmdline, etc
-sudo objcopy \
+objcopy \
     --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
     --add-section .cmdline="/tmp/cmdline" --change-section-vma .cmdline=0x30000 \
     --add-section .splash="config/logo.bmp" --change-section-vma .splash=0x40000 \
@@ -26,16 +27,16 @@ sudo objcopy \
     "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "/tmp/linux.efi"
 
 # Sign the resulting binary
-sudo sbsign --key=/etc/efi-keys/KEK.key --cert=/etc/efi-keys/KEK.crt --output /boot/efi/EFI/debian/VxLinux-signed.efi /tmp/linux.efi
+sbsign --key=/etc/efi-keys/KEK.key --cert=/etc/efi-keys/KEK.crt --output /boot/efi/EFI/debian/VxLinux-signed.efi /tmp/linux.efi
 
 # Now install it 
 EFIDIR="/boot/efi/EFI/debian"
 TARGET="VxLinux-signed.efi"
 OUTDIR="${EFIDIR}/${TARGET}"
-DEV="$(sudo df "$OUTDIR" | tail -1 | cut -d' ' -f1)"
+DEV="$(df "$OUTDIR" | tail -1 | cut -d' ' -f1)"
 part=$(cat /sys/class/block/$(basename $DEV)/partition)
 
-sudo efibootmgr \
+efibootmgr \
 	--quiet \
 	--create \
 	--disk "$DEV" \
