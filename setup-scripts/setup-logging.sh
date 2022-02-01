9edfa7b4866cbe360a1ceb048ce0777fb7e21c06
Matt Bernhard <mdb92nc@gmail.com>
2022-02-01 14:53:01 -0500

    Debugging logging issues

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -1,4 +1,6 @@
+# Good
 sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
+
 sudo cp config/rsyslog.conf /etc/rsyslog.conf
 sudo cp config/journald.conf /etc/systemd/journald.conf
 
