338b0d65fa42134f024e5fe93228f83f0080a69e
Matt Bernhard <mdb92nc@gmail.com>
2022-02-02 15:11:43 -0500

    attempting to create a syslog user

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -1,3 +1,7 @@
+# Check if syslog is a user. If not, add it
+# TODO: 
+id -u syslog  &> /dev/null || sudo useradd -u 104 -U -G adm,tty syslog 
+
 # Good
 sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
 
