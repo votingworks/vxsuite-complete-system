a00f0e04bfc99c1bfa24be94c0c8d1cd2b3916e2
Matt Bernhard <mdb92nc@gmail.com>
2022-02-04 14:27:46 -0500

    Adding more logging permissions

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -3,6 +3,7 @@ if ! id syslog &> /dev/null; then
 	sudo useradd -U -G adm,tty syslog
 	sudo chown syslog:adm /var/spool/rsyslog
 	sudo chown syslog:adm /var/log/syslog
+	sudo chown :adm /var/log
 fi
 
 
