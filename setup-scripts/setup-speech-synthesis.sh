47cae6e8b3fc76ff82cc5c24673dd1d7fcd23158
Matt Bernhard <umbernhard@gmail.com>
2022-02-07 18:39:50 -0500

    Update setup-speech-synthesis.sh
    Newer versions of speech-dispatcher and/or festival use a different path.

--- setup-scripts/setup-speech-synthesis.sh
+++ setup-scripts/setup-speech-synthesis.sh
@@ -13,16 +13,21 @@ sudo apt install -y festival speech-dispatcher speech-dispatcher-festival festvo
 wget http://www.speech.cs.cmu.edu/cmu_arctic/packed/cmu_us_slt_arctic-0.95-release.tar.bz2
 bunzip2 cmu_us_slt_arctic-0.95-release.tar.bz2
 tar xf cmu_us_slt_arctic-0.95-release.tar
-sudo mkdir -p /usr/share/festival/voices/english/
-sudo mv cmu_us_slt_arctic /usr/share/festival/voices/english/cmu_us_slt_arctic_clunits
 
+if [[ $(lsb_release -cs) == "bionic" ]]; then
+  sudo mkdir -p /usr/share/festival/voices/english/
+  sudo mv cmu_us_slt_arctic /usr/share/festival/voices/english/cmu_us_slt_arctic_clunits
+else
+  sudo mkdir -p /usr/share/festival/voices/us/
+	sudo mv cmu_us_slt_arctic /usr/share/festival/voices/us/cmu_us_slt_arctic_clunits
+fi
+  
 # done with downloaded files
 cd "$INSTALL_DIR"
 rm -rf /tmp/vx
 
 # set up festival voice
 sudo cp config/speechd.conf /etc/speech-dispatcher/
-sudo systemctl restart speech-dispatcher
 
 # festival auto-start
 sudo cp config/vx-festival.service /etc/systemd/system/
