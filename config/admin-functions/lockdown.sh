fb803bb531f03b6a3819d586540f3ae8a488b150
Matt Bernhard <umbernhard@gmail.com>
2022-03-04 17:50:48 -0500

    Update lockdown.sh

--- config/admin-functions/lockdown.sh
+++ config/admin-functions/lockdown.sh
@@ -30,12 +30,6 @@ mount /dev/sda /mnt
 sbsign --key=/mnt/DB.key --cert=/mnt/DB.crt --output /boot/efi/EFI/debian/VxLinux-signed.efi /tmp/linux.efi
 
 # Now install it 
-EFIDIR="/boot/efi/EFI/debian"
-TARGET="VxLinux-signed.efi"
-OUTDIR="${EFIDIR}/${TARGET}"
-DEV="$(df "$OUTDIR" | tail -1 | cut -d' ' -f1)"
-part=$(cat /sys/class/block/$(basename $DEV)/partition)
-
 bash setup-boot-entry.sh
 
 # Reboot into the locked down system
