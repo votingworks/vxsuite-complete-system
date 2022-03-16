5ebba25173e28e9474d9804be0fc8d95cc8f8257
Jonah Kagan <jonah@voting.works>
2022-03-16 13:36:33 -0700

    PR feedback

--- config/admin-functions/basic-configuration.sh
+++ config/admin-functions/basic-configuration.sh
@@ -22,15 +22,15 @@ ${VX_FUNCTIONS_ROOT}/set-clock.sh
 echo
 echo -e "\e[1mStep 3: Record Machine Metadata\e[0m"
 echo 'Setting up TOTP...'
-TOTP_URI=`${VX_FUNCTIONS_ROOT}/reset-totp.sh | grep otpauth`
+TOTP_URI=$(${VX_FUNCTIONS_ROOT}/reset-totp.sh | grep otpauth)
 echo "TOTP URI: ${TOTP_URI}"
 echo 'Setting up signing keys...'
 ${VX_FUNCTIONS_ROOT}/generate-key.sh > /dev/null
-PUBLIC_KEY=`cat "${VX_CONFIG_ROOT}/key.pub" | tail -n 1`
+PUBLIC_KEY=$(cat "${VX_CONFIG_ROOT}/key.pub" | tail -n 1)
 echo "Public Signing Key: ${PUBLIC_KEY}"
 echo "Record this QR code containing the Machine ID, TOTP URI, and Public Signing Key:"
-MACHINE_ID=`cat "${VX_CONFIG_ROOT}/machine-id"`
-echo -e "${MACHINE_ID}\n${TOTP_URI}\n${PUBLIC_KEY}" | qrencode -t ANSIUTF8 -o -
+MACHINE_ID=$(< "${VX_CONFIG_ROOT}/machine-id")
+echo -e "${MACHINE_ID}\n${TOTP_URI}\n${PUBLIC_KEY}" | qrencode -t ANSI -o -
 
 while true; do
     read -p "Confirm QR code recorded (y/n) " CONFIRM
@@ -40,4 +40,4 @@ done
 echo
 echo -e "\e[1mBasic Configuration Complete\e[0m"
 read -p "You must reboot for these changes to take effect. Reboot now? (y/n) " CONFIRM
-[[ "${CONFIRM}" = "y" ]] && sudo reboot
+[[ "${CONFIRM}" = "y" ]] && systemctl reboot -i
