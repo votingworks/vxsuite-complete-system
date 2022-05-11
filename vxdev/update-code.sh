41b7b1861e112b32cab06d14b932ffaeff6b192a
Caroline Mars <caroline@voting.works>
2022-05-10 17:01:59 -0700

    fix git output from showing up in terminal

--- vxdev/update-code.sh
+++ vxdev/update-code.sh
@@ -5,7 +5,7 @@
 APP_TYPE=$(sudo cat "$VX_CONFIG_ROOT/machine-type")
 
 cd /vx/code/vxsuite-complete-system
-git checkout main > /dev/null
+git checkout main > /dev/null 2>&1
 git pull > /dev/null
 git fetch --tags > /dev/null
 sudo git clean -xfd > /dev/null
