07dcc77ffcf8969b600f7619655f1e53f8f20400
Ben Adida <ben@adida.net>
2022-05-24 12:28:21 -0400

    run admin service with election-manager, bump vxsuite to proper version that includes necessary build files.

--- run-scripts/run-election-manager.sh
+++ run-scripts/run-election-manager.sh
@@ -11,4 +11,4 @@ source ${CONFIG}/read-vx-machine-config.sh
 
 export PIPENV_VENV_IN_PROJECT=1
 export NODE_ENV=production
-(trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/services/smartcards run & make -C vxsuite/services/converter-ms-sems run & make -C vxsuite/frontends/election-manager run) | logger --tag votingworksapp
+(trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/services/admin run & make -C vxsuite/services/smartcards run & make -C vxsuite/services/converter-ms-sems run & make -C vxsuite/frontends/election-manager run) | logger --tag votingworksapp
