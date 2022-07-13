21eba9e8ada0a227666c89a8e6264fc7d6b5505b
Matt Bernhard <mdb92nc@gmail.com>
2022-07-13 11:39:16 -0400

    updating comments in sign.sh to be more accuratE

--- config/admin-functions/sign.sh
+++ config/admin-functions/sign.sh
@@ -6,7 +6,7 @@ tpm2 startauthsession -Q -S session.ctx --policy-session
 tpm2_policypcr -Q -S session.ctx -l "sha256:0,2,4,5,7" --policy pcr.policy
 
 # This expects the script to be run with the data to be signed passed in via 
-# a here-string, e.g. sudo ./sign.sh <<< $(cat data.blob).
+# a here-string, e.g. sudo ./sign.sh <<< "a string"
 # It "returns" the signature on stdout by catting a temporary file. 
 tpm2 sign -Q -p session:session.ctx -c 0x81000001 -f plain -o /tmp/sig 
 
