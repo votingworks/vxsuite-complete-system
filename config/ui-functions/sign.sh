ffcdc04589b6a1fed916c7549c35d618250f4863
Matt Bernhard <mdb92nc@gmail.com>
2022-07-27 15:20:23 -0400

    Auto-detect TPM PCR algorithm

--- config/ui-functions/sign.sh
+++ config/ui-functions/sign.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 set -euo pipefail
+# First detect whether the TPM supports sha1 or sha256
+algo="sha256"
+if [ $(tpm2 pcrread sha1:0 | wc -l) == 2 ]; then
+    algo="sha1"
+fi
 
 tpm2 startauthsession -Q -S session.ctx --policy-session
-tpm2_policypcr -Q -S session.ctx -l "sha256:0,7" --policy pcr.policy
+tpm2_policypcr -Q -S session.ctx -l "${algo}:0,7" --policy pcr.policy
 
 # This expects the script to be run with the data to be signed passed in via 
 # a here-string, e.g. sudo ./sign.sh <<< "a string"
