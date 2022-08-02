ede32ec396b06e04c6bbc9ac87051eb442032770
Matt Bernhard <mdb92nc@gmail.com>
2022-08-02 17:12:27 -0400

    Working on setting up surface go boot entry

--- config/admin-functions/lockdown.sh
+++ config/admin-functions/lockdown.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 : "${VX_FUNCTIONS_ROOT:="$(dirname "$0")"}"
 
+# detect Surface Go
+if dmidecode | grep -q 'Surface Go'; then
+    surface=1
+else
+    surface=0
+fi
+
 update-initramfs -u
 
 # Remount / so it can't change while we're doing the veritysetup
@@ -17,22 +24,31 @@ echo "$(cat /vx/admin/config/cmdline)${HASH}" > /tmp/cmdline
 
 KERNEL_VERSION=`uname -r`
 
-# Now package up our kernel, cmdline, etc
-objcopy \
-    --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
-    --add-section .cmdline="/tmp/cmdline" --change-section-vma .cmdline=0x30000 \
-    --add-section .splash="/vx/admin/config/logo.bmp" --change-section-vma .splash=0x40000 \
-    --add-section .linux="/boot/vmlinuz-${KERNEL_VERSION}" --change-section-vma .linux=0x2000000 \
-    --add-section .initrd="/boot/initrd.img-${KERNEL_VERSION}" --change-section-vma .initrd=0x3000000 \
-    "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "/tmp/linux.efi"
 
-# Sign the resulting binary
-# First make sure the drive is mounted
-umount /dev/sda || true
-umount /dev/sda1 || true
-mount /dev/sda /mnt || mount /dev/sda1 /mnt || (echo "Secure boot keys not found; exiting" && sleep 5 && exit);
+# Now package up our kernel, cmdline, etc, if we're not on a Surface
+if [ $surface == 0 ]]; then
+    objcopy \
+        --add-section .osrel="/usr/lib/os-release" --change-section-vma .osrel=0x20000 \
+        --add-section .cmdline="/tmp/cmdline" --change-section-vma .cmdline=0x30000 \
+        --add-section .splash="/vx/admin/config/logo.bmp" --change-section-vma .splash=0x40000 \
+        --add-section .linux="/boot/vmlinuz-${KERNEL_VERSION}" --change-section-vma .linux=0x2000000 \
+        --add-section .initrd="/boot/initrd.img-${KERNEL_VERSION}" --change-section-vma .initrd=0x3000000 \
+        "/usr/lib/systemd/boot/efi/linuxx64.efi.stub" "/tmp/linux.efi"
 
-sbsign --key=/mnt/DB.key --cert=/mnt/DB.crt --output /boot/efi/EFI/debian/VxLinux-signed.efi /tmp/linux.efi
+    # Sign the resulting binary
+    # First make sure the drive is mounted
+    umount /dev/sda || true
+    umount /dev/sda1 || true
+    mount /dev/sda /mnt || mount /dev/sda1 /mnt || (echo "Secure boot keys not found; exiting" && sleep 5 && exit);
+
+    sbsign --key=/mnt/DB.key --cert=/mnt/DB.crt --output /boot/efi/EFI/debian/VxLinux-signed.efi /tmp/linux.efi
+else
+    # On a surface we just need to setup the right GRUB entry
+    echo "menuentry \"VxLinux\" {
+       initrd /initrd.img-${KERNEL_VERSION} 
+       linux /vmlinuz-${KERNEL_VERSION} $(cat /tmp/cmdline)
+}" >> /etc/grub.d/40_custom
+fi
 
 # Now install it 
 bash "${VX_FUNCTIONS_ROOT}/setup-boot-entry.sh"
