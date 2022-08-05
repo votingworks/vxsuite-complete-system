e17cec0ab2979a082e9431ad675d7ab433ea360b
Matt Bernhard <umbernhard@gmail.com>
2022-08-05 13:59:32 -0400

    Using machine type to decide type of lock down, with prompt. (#223)

--- config/admin-functions/lockdown.sh
+++ config/admin-functions/lockdown.sh
@@ -5,11 +5,25 @@ set -euo pipefail
 : "${VX_CONFIG_ROOT:="/vx/config"}"
 
 # detect Surface Go
-if dmidecode | grep -q 'Surface Go'; then
+#if dmidecode | grep -q 'Surface Go'; then
+if [[ $(cat "${VX_CONFIG_ROOT}/machine-type") == "precinct-scanner" ]]; then
     surface=1
+    echo "Detected a Precinct Scanner (Surface Go) device. Locking down with GRUB."
 else
     surface=0
+    echo "Locking down with a unified kernel binary."
 fi
+
+echo "continue? [Y/n]:" 
+
+read -r answer
+
+if [[ $answer != 'n' && $answer != 'N' ]]; then
+    echo "Not locking down. Exiting..."
+    sleep 3
+    exit
+fi
+
 
 update-initramfs -u
 
