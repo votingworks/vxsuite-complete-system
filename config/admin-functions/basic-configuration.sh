89e898ca2dcf5d12bd64bd3b9efdb25c6104a252
arsalansufi <arsalansufi@gmail.com>
2023-04-12 13:21:06 -0400

    Add machine cert creation step to basic configuration wizard (#283)
    * Add machine cert creation step to basic configuration wizard
    
    * Auto-generate private key password instead of getting from user input
    
    * Skip machine cert creation for now
    
    Until I've added support for bootstrapping your first sys admin
    card

--- config/admin-functions/basic-configuration.sh
+++ config/admin-functions/basic-configuration.sh
@@ -35,6 +35,11 @@ while true; do
     [[ "${CONFIRM}" = "y" ]] && break
 done
 
+# Will uncomment this as soon as I've added support for bootstrapping your first sys admin card
+# echo
+# echo -e "\e[1mStep 4: Create Machine Cert\e[0m"
+# sudo ${VX_FUNCTIONS_ROOT}/create-machine-cert.sh
+
 if [[ -f "${VX_CONFIG_ROOT}/RUN_BASIC_CONFIGURATION_ON_NEXT_BOOT" ]]; then
     rm -f "${VX_CONFIG_ROOT}/RUN_BASIC_CONFIGURATION_ON_NEXT_BOOT"
 fi
