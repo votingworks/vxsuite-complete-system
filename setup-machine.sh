fe24e275b0beb2bb0d7d1abf624e08101a1bdcc6
Arsalan Sufi <arsalan@voting.works>
2023-04-17 15:58:28 -0400

    Fix 'sudo: unable to resolve host VotingWorks' errors

--- setup-machine.sh
+++ setup-machine.sh
@@ -395,6 +395,7 @@ sudo passwd -l vx-services
 
 # set a clean hostname
 sudo hostnamectl set-hostname "VotingWorks"
+sudo sh -c 'echo "\n127.0.1.1\tVotingWorks" >> /etc/hosts'
 
 # move in our sudo file, which removes sudo'ing except for granting vx-admin a very specific set of privileges
 if [[ $VXADMIN_SUDO ]] ; then
