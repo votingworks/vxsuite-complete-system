600991e9b7b0b68105d4bd68609cd01fd51f3280
Matt Bernhard <umbernhard@gmail.com>
2023-05-02 13:35:09 -0400

    Actually piping stuff correctly.

--- config/admin-functions/generate-key.sh
+++ config/admin-functions/generate-key.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 : "${VX_CONFIG_ROOT:="/vx/config"}"
 
-
-set -euo pipefail 
-
 # First detect whether the TPM supports sha1 or sha256
 algo="sha256"
 if [ $(tpm2 pcrread sha1:0 | wc -l) == 2 ]; then
@@ -20,7 +17,7 @@ tpm2_createprimary -C o -c primary.ctx
 # Save the primary context for reuse after reboot
 # First make sure nothing is in the handle we want to save our primary context
 # to
-tpm2_evictcontrol -Q -c 0x81000000 || true 2>&1 > /dev/null
+tpm2_evictcontrol -Q -c 0x81000000 &> /dev/null || true
 tpm2_evictcontrol -c primary.ctx 0x81000000
 
 # Create keys
@@ -32,7 +29,7 @@ tpm2_create -L pcr.policy -u key.pub -r key.priv -C primary.ctx -G ecc
 tpm2_load -u key.pub -r key.priv -C primary.ctx -c key.ctx
 
 # Save the keys
-tpm2_evictcontrol -Q -c 0x81000001 || true 2>&1 > /dev/null
+tpm2_evictcontrol -Q -c 0x81000001 &> /dev/null || true
 tpm2_evictcontrol -c key.ctx 0x81000001
 
 # Delete the local key files
