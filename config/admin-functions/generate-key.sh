a31589a7f3c17895095ce331ce2bb07377005ce7
Matt Bernhard <umbernhard@gmail.com>
2023-05-02 13:23:21 -0400

    Actually pipe error message to /dev/null

--- config/admin-functions/generate-key.sh
+++ config/admin-functions/generate-key.sh
@@ -20,7 +20,7 @@ tpm2_createprimary -C o -c primary.ctx
 # Save the primary context for reuse after reboot
 # First make sure nothing is in the handle we want to save our primary context
 # to
-tpm2_evictcontrol -Q -c 0x81000000 || true 2>&1 /dev/null
+tpm2_evictcontrol -Q -c 0x81000000 || true 2>&1 > /dev/null
 tpm2_evictcontrol -c primary.ctx 0x81000000
 
 # Create keys
@@ -32,7 +32,7 @@ tpm2_create -L pcr.policy -u key.pub -r key.priv -C primary.ctx -G ecc
 tpm2_load -u key.pub -r key.priv -C primary.ctx -c key.ctx
 
 # Save the keys
-tpm2_evictcontrol -Q -c 0x81000001 || true 2>&1 /dev/null
+tpm2_evictcontrol -Q -c 0x81000001 || true 2>&1 > /dev/null
 tpm2_evictcontrol -c key.ctx 0x81000001
 
 # Delete the local key files
