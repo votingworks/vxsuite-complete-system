89d8028ec358a3e8cd917128f4ac6be55f3ce91a
Arsalan Sufi <arsalan@voting.works>
2023-05-11 10:37:09 -0400

    Install OpenSSL TPM engine

--- setup-scripts/setup-tpm2-tools.sh
+++ setup-scripts/setup-tpm2-tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 set -euo pipefail 
 
-sudo apt -y install tpm2-tools qrencode
+# Where libengine-tpm2-tss-openssl lives
+sudo sh -c 'echo "\ndeb http://http.us.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list'
+sudo apt update
 
+sudo apt -y install tpm2-tools qrencode libengine-tpm2-tss-openssl
