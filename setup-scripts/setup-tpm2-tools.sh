0dc4f14f01ebaed0b62874176c35c88202d6ab25
Arsalan Sufi <arsalan@voting.works>
2023-05-15 11:42:51 -0400

    Ensure that we only pull from bullseye-backports when necessary

--- setup-scripts/setup-tpm2-tools.sh
+++ setup-scripts/setup-tpm2-tools.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail 
 
-# Where libengine-tpm2-tss-openssl lives
+# libengine-tpm2-tss-openssl is found in bullseye-backports, which we add here but assign the
+# lowest possible priority to, to ensure that we only pull from it when absolutely necessary
 sudo sh -c 'echo "\ndeb http://http.us.debian.org/debian bullseye-backports main" >> /etc/apt/sources.list'
+sudo sh -c 'echo "Package: *\nPin: release a=bullseye-backports\nPin-Priority: 1" >> /etc/apt/preferences.d/bullseye-backports'
 sudo apt update
 
-sudo apt -y install tpm2-tools qrencode libengine-tpm2-tss-openssl
+sudo apt -y install tpm2-tools qrencode
+sudo apt -y -t bullseye-backports install libengine-tpm2-tss-openssl
