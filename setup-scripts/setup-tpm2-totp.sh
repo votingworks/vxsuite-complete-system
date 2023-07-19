8beb61845aafb9172983970962b58736e115ebc4
Adam McManus <adam.mcmanus@gmail.com>
2023-07-19 14:22:44 -0500

    remove apt installs

--- setup-scripts/setup-tpm2-totp.sh
+++ setup-scripts/setup-tpm2-totp.sh
@@ -5,9 +5,6 @@ if hostnamectl status | grep 'Virtualization: parallels'; then
         exit 0	
 fi
 
-# install core dependencies
-sudo apt -y install build-essential autoconf autoconf-archive automake m4 libtool gcc pkg-config libqrencode-dev libssl-dev libjson-c-dev libcurl4-openssl-dev
-
 (
     cd tpm2-software/tpm2-tss
     ./bootstrap
