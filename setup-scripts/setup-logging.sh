28d08ef358522541673cdee74333f308c2b7c6e2
Adam McManus <adam.mcmanus@gmail.com>
2023-11-06 16:26:57 -0600

    move user management to build-system

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -1,10 +1,3 @@
-# Check if syslog is a user. If not, add it
-if ! id syslog &> /dev/null; then
-	sudo useradd -U -G adm,tty syslog
-fi
-
-sudo chown syslog:adm /var/spool/rsyslog
-
 sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
 sudo cp config/rsyslog.conf /etc/rsyslog.conf
 sudo cp config/journald.conf /etc/systemd/journald.conf
