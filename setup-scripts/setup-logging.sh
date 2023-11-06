3c4d155ca256e86c9b0124021793424e7e8eab80
Adam McManus <adam.mcmanus@gmail.com>
2023-11-06 14:04:14 -0600

    truncate system logs so they dont contain all the build activity

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -11,4 +11,11 @@ sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
 sudo cp config/rsyslog.conf /etc/rsyslog.conf
 #sudo cp config/journald.conf /etc/systemd/journald.conf
 
+# Truncate existing log files so build operations are not
+# part of production use
+for file in mail.log kern.log auth.log user.log cron.log
+do
+  sudo sh -c "cat /dev/null > /var/log/${file}"
+done
+
 sudo systemctl restart rsyslog
