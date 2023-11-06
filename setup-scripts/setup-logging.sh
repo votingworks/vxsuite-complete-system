a2e7c2c142abee32c9efcf63fea92a4454bd2b84
Adam McManus <adam.mcmanus@gmail.com>
2023-11-06 15:09:25 -0600

    change ownership of system logs

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -8,14 +8,15 @@ if ! id syslog &> /dev/null; then
 fi
 
 sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
-#sudo cp config/rsyslog.conf /etc/rsyslog.conf
-sudo cp config/journald.conf /etc/systemd/journald.conf
+sudo cp config/rsyslog.conf /etc/rsyslog.conf
+#sudo cp config/journald.conf /etc/systemd/journald.conf
 
 # Truncate existing log files so build operations are not
 # part of production use
 for file in mail.log kern.log user.log cron.log syslog auth.log
 do
   sudo sh -c "cat /dev/null > /var/log/${file}"
+  sudo chown syslog:adm /var/log/${file}
 done
 
 sudo systemctl restart rsyslog
