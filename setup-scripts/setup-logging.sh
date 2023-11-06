f099c42d4ca2d83cb4f7099204f059723ab1d2d8
Adam McManus <adam.mcmanus@gmail.com>
2023-11-06 14:59:16 -0600

    try default rsyslog.conf

--- setup-scripts/setup-logging.sh
+++ setup-scripts/setup-logging.sh
@@ -8,12 +8,12 @@ if ! id syslog &> /dev/null; then
 fi
 
 sudo cp config/30-votingworks.conf /etc/rsyslog.d/30-votingworks.conf
-sudo cp config/rsyslog.conf /etc/rsyslog.conf
-#sudo cp config/journald.conf /etc/systemd/journald.conf
+#sudo cp config/rsyslog.conf /etc/rsyslog.conf
+sudo cp config/journald.conf /etc/systemd/journald.conf
 
 # Truncate existing log files so build operations are not
 # part of production use
-for file in mail.log kern.log auth.log user.log cron.log
+for file in mail.log kern.log user.log cron.log syslog auth.log
 do
   sudo sh -c "cat /dev/null > /var/log/${file}"
 done
