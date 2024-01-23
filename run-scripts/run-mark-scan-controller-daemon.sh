544d2400cef778cbeaf106ff484dd7b4a72ce240
Kevin Shen <kevin@voting.works>
2024-01-23 12:22:26 -0800

    add service configs and run scripts

---
+++ run-scripts/run-mark-scan-controller-daemon.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# go to directory where this file is located
+cd "$(dirname "$0")"
+
+# configuration information
+CONFIG=${VX_CONFIG_ROOT:-./config}
+METADATA=${VX_METADATA_ROOT:-./}
+source ${CONFIG}/read-vx-machine-config.sh
+
+export PIPENV_VENV_IN_PROJECT=1
+export NODE_ENV=production
+(trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/apps/mark-scan/accessible-controller run) | logger --tag votingworksapp
