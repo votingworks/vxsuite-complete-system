3da61fcbfd73159628eed27d3a4d336cfc9c38c0
Kevin Shen <kevin@voting.works>
2024-01-23 13:15:28 -0800

    remove unused language config

--- run-scripts/run-mark-scan-pat-daemon.sh
+++ run-scripts/run-mark-scan-pat-daemon.sh
@@ -10,6 +10,4 @@ CONFIG=${VX_CONFIG_ROOT:-./config}
 METADATA=${VX_METADATA_ROOT:-./}
 source ${CONFIG}/read-vx-machine-config.sh
 
-export PIPENV_VENV_IN_PROJECT=1
-export NODE_ENV=production
 (trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/apps/mark-scan/backend run-pat-daemon) | logger --tag votingworksapp
