4d7788ae7ecf86fbd85a96fc0241e9adddbfda36
Andrew Hayes <adghayes@gmail.com>
2024-02-09 16:06:10 -0800

    disable kiosk printing for all but mark

--- run-scripts/run-kiosk-browser.sh
+++ run-scripts/run-kiosk-browser.sh
@@ -3,16 +3,24 @@
 set -euo pipefail
 
 URL=${1:-http://localhost:3000}
-: "${VX_CONFIG_ROOT:="./config"}"
-: "${VX_METADATA_ROOT:="./"}"
-
-OS=$(lsb_release -cs)
 PRINTER_FILE='./printing/debian-printer-autoconfigure.json'
 
-kiosk-browser \
-  --add-file-perm o=http://localhost:3000,p=/media/**/*,rw \
-  --add-file-perm o=http://localhost:3000,p=/var/log,ro \
-  --add-file-perm o=http://localhost:3000,p=/var/log/*,ro \
-  --autoconfigure-print-config ${PRINTER_FILE} \
-  --url ${URL} || true
-
+case "${VX_MACHINE_TYPE:-}" in
+  scan|admin|central-scan)
+    # for these apps, kiosk browser does not need to or should not configure printers
+    kiosk-browser \
+      --add-file-perm o=http://localhost:3000,p=/media/**/*,rw \
+      --add-file-perm o=http://localhost:3000,p=/var/log,ro \
+      --add-file-perm o=http://localhost:3000,p=/var/log/*,ro \
+      --url ${URL} || true
+    ;;
+  *)
+    # by default, for mark and development, kiosk browser handles configuring printers
+    kiosk-browser \
+      --add-file-perm o=http://localhost:3000,p=/media/**/*,rw \
+      --add-file-perm o=http://localhost:3000,p=/var/log,ro \
+      --add-file-perm o=http://localhost:3000,p=/var/log/*,ro \
+      --autoconfigure-print-config ${PRINTER_FILE} \
+      --url ${URL} || true
+    ;;
+esac
