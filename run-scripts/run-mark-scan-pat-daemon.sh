6286755355f4f1057f49d2096f28db40521a8f66
Kevin Shen <kwshen@gmail.com>
2024-03-19 09:49:18 -0700

    add service configs and run scripts (#364)
    * add service configs and run scripts
    
    * remove unused language config
    
    * update PAT input run command

---
+++ run-scripts/run-mark-scan-pat-daemon.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# go to directory where this file is located
+cd "$(dirname "$0")"
+
+# configuration information
+CONFIG=${VX_CONFIG_ROOT:-./config}
+METADATA=${VX_METADATA_ROOT:-./}
+source ${CONFIG}/read-vx-machine-config.sh
+
+(trap 'kill 0' SIGINT SIGHUP; make -C vxsuite/apps/mark-scan/pat-device-input run) | logger --tag votingworksapp
