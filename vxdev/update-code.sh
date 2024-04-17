6d4e08c0b9bca7efbb3633c4f4f12a7900e9c41d
Adam McManus <adam.mcmanus@gmail.com>
2024-04-17 10:38:32 -0500

    updating mark-scan should always ensure daemon service files are copied and daemon config reloaded

--- vxdev/update-code.sh
+++ vxdev/update-code.sh
@@ -98,6 +98,14 @@ if [[ $APP_TYPE == 'VxMarkScan' ]]; then
 	cp /vx/config/.env.local vxsuite/apps/mark-scan/frontend/.env.local
 	./prepare_build.sh mark-scan
 	./build.sh mark-scan
+	for vx_daemon in controller pat
+	do
+	  sudo cp config/mark-scan-${vx_daemon}-daemon.service /etc/systemd/system/
+	  sudo cp run-scripts/run-mark-scan-${vx_daemon}-daemon.sh /vx/code/
+	  sudo chmod 644 /etc/systemd/system/mark-scan-${vx_daemon}-daemon.service
+	  sudo ln -sf /vx/code/run-mark-scan-${vx_daemon}-daemon.sh /vx/services/run-mark-scan-${vx_daemon}-daemon.sh
+	  sudo systemctl daemon-reload
+	done
 fi
 
 echo "Done! Closing in 3 seconds."
