670a7c7358e17efb7260eea34b32dfce4af10b4c
Adam McManus <adam.mcmanus@gmail.com>
2024-05-09 13:30:40 -0500

    Updates for signed hash validation (#370)
    * add noninteractive mode to hash-signature.sh and grant sudo access to it for the vx-services user
    
    * add to sudoers-for-dev

--- config/admin-functions/hash-signature.sh
+++ config/admin-functions/hash-signature.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Retrieving image signature (this may take some time)..."
-sha256sum /dev/mapper/Vx--vg-root
-echo ""
-
-read -p "Press enter once you have recorded the image signature."
+if [[ $1 == "noninteractive" ]]; then
+  hash=$(sha256sum /dev/mapper/Vx--vg-root | cut -d' ' -f1)
+  echo "$hash"
+else
+  echo "Retrieving image signature (this may take some time)..."
+  sha256sum /dev/mapper/Vx--vg-root
+  echo ""
+  read -p "Press enter once you have recorded the image signature."
+fi
 
 exit 0;
