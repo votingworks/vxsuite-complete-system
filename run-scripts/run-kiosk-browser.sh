64d80339b24b1f6d0dde4cc0086e5fa90f7bd0fd
Andrew Hayes <Adghayes@gmail.com>
2024-05-16 16:21:32 -0700

    remove a lot of kiosk-browser functionality (#371)

--- run-scripts/run-kiosk-browser.sh
+++ run-scripts/run-kiosk-browser.sh
@@ -7,12 +7,10 @@ URL=${1:-http://localhost:3000}
 : "${VX_METADATA_ROOT:="./"}"
 
 OS=$(lsb_release -cs)
-PRINTER_FILE='./printing/debian-printer-autoconfigure.json'
 
 kiosk-browser \
   --add-file-perm o=http://localhost:3000,p=/media/**/*,rw \
   --add-file-perm o=http://localhost:3000,p=/var/log,ro \
   --add-file-perm o=http://localhost:3000,p=/var/log/*,ro \
-  --autoconfigure-print-config ${PRINTER_FILE} \
   --url ${URL} || true
 
