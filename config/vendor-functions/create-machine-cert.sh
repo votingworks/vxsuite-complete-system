5bad1a6088d7dd670400368b1cdb8d31e523bf65
arsalansufi <arsalan@voting.works>
2024-11-19 11:50:04 -0500

    Handle slight clock divergences between machines during cert check (#419)

--- config/vendor-functions/create-machine-cert.sh
+++ config/vendor-functions/create-machine-cert.sh
@@ -123,8 +123,12 @@ if ! openssl x509 -in "${MACHINE_CERT_PATH}" -noout -pubkey | \
 fi
 
 # Cert correctness check 2
-if ! openssl verify -CAfile "${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem" "${MACHINE_CERT_PATH}" > /dev/null; then
-    echo -e "\e[31mMachine cert was not signed by the correct cert authority\e[0m" >&2
+if ! openssl verify \
+    # Accept a cert that has a start time slightly in the future because the clock on VxCertifier
+    # was slightly ahead of the machine clock
+    -attime "$(date -d "+10 minutes" +%s)" \
+    -CAfile "${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem" "${MACHINE_CERT_PATH}" > /dev/null; then
+    echo -e "\e[31mMachine cert was not signed by the correct cert authority or is not yet valid because of a clock mismatch\e[0m" >&2
     read -p "Press enter to start over. "
     exit 1
 fi
