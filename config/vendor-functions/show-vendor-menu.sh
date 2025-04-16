90e1fda4e94a1fde8a0678217f2dc3e2776fa5c0
Adam McManus <adam.mcmanus@gmail.com>
2025-04-16 13:50:10 -0500

    force a reboot after var expansion due to current autologin config

--- config/vendor-functions/show-vendor-menu.sh
+++ config/vendor-functions/show-vendor-menu.sh
@@ -18,6 +18,12 @@ fi
 # on systems installed to physical hardware.
 if [[ $(tty) = /dev/tty1 ]] && [[ -f "${VX_CONFIG_ROOT}/EXPAND_VAR" ]]; then
   sudo "${VX_FUNCTIONS_ROOT}/expand-var-filesystem.sh"
+
+  # Note: we should improve this in the future. There's no filesystem need
+  # to reboot. We're only doing so because of our autologin configuration.
+  echo "The /var filesystem has been resized. The system will now reboot."
+  sleep 3
+  sudo /usr/sbin/reboot
 fi
 
 if [[ $(tty) = /dev/tty1 ]] && [[ -f "${VX_CONFIG_ROOT}/RUN_BASIC_CONFIGURATION_ON_NEXT_BOOT" ]]; then
