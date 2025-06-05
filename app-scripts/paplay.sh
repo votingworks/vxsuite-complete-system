23d6e66186f66bf15b3bf2f5b0f189da57d4780e
Kofi <264902+kofi-q@users.noreply.github.com>
2025-06-05 16:21:13 -0500

    Add paplay.sh app script for playing audio through specific outputs (#448)

---
+++ app-scripts/paplay.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage () {
+  echo "Usage: paplay <options>"
+  exit 1
+}
+
+if [[ $# -eq 0 ]]; then
+  usage
+fi
+
+vx_ui_id=$( id -u vx-ui )
+
+sudo -u vx-ui XDG_RUNTIME_DIR=/run/user/${vx_ui_id} paplay $@
