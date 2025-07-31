e08d73b01503c3f6745d9a5d877e019e5febaa6b
Adam McManus <adam.mcmanus@gmail.com>
2025-07-31 17:26:31 -0500

    include the hwta script

--- vxdev/vxdev-configuration.sh
+++ vxdev/vxdev-configuration.sh
@@ -13,6 +13,7 @@ sudo cp vxdev/runprogram.png /home/vx/.icons
 sudo cp vxdev/votingworks-desktop.png /vx/.
 
 # Copy scripts and desktop files into the appropriate places
+sudo cp vxdev/set-hwta-env.sh /vx/scripts/.
 sudo cp vxdev/update-code.sh /vx/scripts/.
 sudo cp vxdev/update-vxdev.sh /vx/scripts/.
 sudo cp vxdev/update-code.desktop /usr/share/applications/.
