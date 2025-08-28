55108790d94f7867dd7263a98b2bc52a2abd88dd
Arsalan Sufi <arsalan@voting.works>
2025-08-28 08:41:14 -0400

    Add support for batched VxCertifier requests (and other improvements)

--- config/vendor-functions/create-machine-cert.sh
+++ config/vendor-functions/create-machine-cert.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # Requires sudo
-# TODO: With the introduction of pollbook certs, we are duplicating
-# some aspects of cert creation / validation. That's fine for now, but
-# if this pattern continues, we should consider whether to make this
-# more flexible with less duplication
 
 set -euo pipefail
 
@@ -15,20 +11,47 @@ set -euo pipefail
 : "${VX_MACHINE_ID:="$(< "${VX_CONFIG_ROOT}/machine-id")"}"
 : "${IS_QA_IMAGE:="$(< "${VX_CONFIG_ROOT}/is-qa-image")"}"
 
-USE_STRONGSWAN_TPM_KEY="0"
+ROOT_VX_CERT_AUTHORITY_CERT_PATH="${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem"
+
+USB_DRIVE_CERTS_DIRECTORY="/media/vx/usb-drive/certs"
+USB_DRIVE_CSR_PATH="${USB_DRIVE_CERTS_DIRECTORY}/csr-${VX_MACHINE_ID}.pem"
+USB_DRIVE_CERT_PATH="${USB_DRIVE_CERTS_DIRECTORY}/cert-${VX_MACHINE_ID}.pem"
 
 if [[ "${VX_MACHINE_TYPE}" == "admin" || "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
     MACHINE_CERT_PATH="${VX_CONFIG_ROOT}/vx-${VX_MACHINE_TYPE}-cert-authority-cert.pem"
-    if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-      STRONGSWAN_X509_PATH="/etc/swanctl/x509/vx-poll-book-strongswan-rsa-cert.pem"
-      STRONGSWAN_CA_PATH="/etc/swanctl/x509ca/vx-cert-authority-cert.pem"
-    fi
 else
     MACHINE_CERT_PATH="${VX_CONFIG_ROOT}/vx-${VX_MACHINE_TYPE}-cert.pem"
 fi
 
-USB_DRIVE_CERTS_DIRECTORY="/media/vx/usb-drive/certs"
-VX_IANA_ENTERPRISE_OID="1.3.6.1.4.1.59817"
+if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
+    USB_DRIVE_STRONGSWAN_CSR_PATH="${USB_DRIVE_CERTS_DIRECTORY}/csr-${VX_MACHINE_ID}-strongswan.pem"
+    USB_DRIVE_STRONGSWAN_CERT_PATH="${USB_DRIVE_CERTS_DIRECTORY}/cert-${VX_MACHINE_ID}-strongswan.pem"
+    MACHINE_STRONGSWAN_CERT_PATH="/etc/swanctl/x509/vx-poll-book-strongswan-rsa-cert.pem"
+fi
+
+USE_STRONGSWAN_TPM_KEY="0"
+
+function select_usb_drive_and_mount() {
+    "${VX_FUNCTIONS_ROOT}/select-usb-drive-and-mount.sh"
+}
+
+function unmount_usb_drive() {
+    "${VX_METADATA_ROOT}/app-scripts/unmount-usb.sh"
+}
+
+# Don't delete the entire certs directory on the USB drive, just the files relevant to the current
+# machine ID
+function clean_up_usb_drive() {
+    mkdir -p "${USB_DRIVE_CERTS_DIRECTORY}"
+    rm -rf "${USB_DRIVE_CSR_PATH}" "${USB_DRIVE_CERT_PATH}"
+    if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
+        rm -rf "${USB_DRIVE_STRONGSWAN_CSR_PATH}" "${USB_DRIVE_STRONGSWAN_CERT_PATH}"
+    fi
+}
+
+#
+# Write cert signing request(s) to USB drive
+#
 
 function get_machine_jurisdiction_from_user_input() {
     local machine_jurisdiction
@@ -70,41 +93,30 @@ function create_machine_cert_signing_request() {
     popd > /dev/null
 }
 
-function unmount_usb_drive() {
-    "${VX_METADATA_ROOT}/app-scripts/unmount-usb.sh"
-}
-
-# Applies permissions to match the permissions of other non-executable files in VX_CONFIG_ROOT
-function match_vx_config_non_executable_file_permissions() {
-    local file_path="${1}"
-    chown vx-vendor:vx-group "${file_path}"
-    chmod u=rw,g=r,o= "${file_path}"
-}
-
 mkdir -p "${VX_CONFIG_ROOT}"
 
 if [[ "${VX_MACHINE_TYPE}" == "admin" || "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-    machine_jurisdiction="$(get_machine_jurisdiction_from_user_input)"
+    MACHINE_JURISDICTION="$(get_machine_jurisdiction_from_user_input)"
 fi
 
 read -p "Insert a USB drive into the machine. Press enter once you've done so. "
-"${VX_FUNCTIONS_ROOT}/select-usb-drive-and-mount.sh"
+select_usb_drive_and_mount
+clean_up_usb_drive
 
-echo "Writing cert signing request to USB drive..."
-rm -rf "${USB_DRIVE_CERTS_DIRECTORY}"
-mkdir "${USB_DRIVE_CERTS_DIRECTORY}"
+echo "Writing cert signing request(s) to USB drive..."
+
 if [[ "${VX_MACHINE_TYPE}" == "admin" || "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-    create_machine_cert_signing_request "${machine_jurisdiction}" > "${USB_DRIVE_CERTS_DIRECTORY}/csr.pem"
-    
-    # Pollbooks need an additional cert for strongswan using a different TPM handle
-    if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-      USE_STRONGSWAN_TPM_KEY="1"
-      create_machine_cert_signing_request "${machine_jurisdiction}" > "${USB_DRIVE_CERTS_DIRECTORY}/vx-poll-book-strongswan-csr.pem"
-    fi
+    create_machine_cert_signing_request "${MACHINE_JURISDICTION}" > "${USB_DRIVE_CSR_PATH}"
 else
-    create_machine_cert_signing_request > "${USB_DRIVE_CERTS_DIRECTORY}/csr.pem"
+    create_machine_cert_signing_request > "${USB_DRIVE_CSR_PATH}"
 fi
-echo "${VX_MACHINE_TYPE}" > "${USB_DRIVE_CERTS_DIRECTORY}/machine-type"
+
+# VxPollBooks need an additional cert for strongSwan using a different TPM handle
+if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
+    USE_STRONGSWAN_TPM_KEY="1"
+    create_machine_cert_signing_request "${MACHINE_JURISDICTION}" > "${USB_DRIVE_STRONGSWAN_CSR_PATH}"
+fi
+
 unmount_usb_drive
 
 if [[ "${IS_QA_IMAGE}" == 1 ]]; then
@@ -117,9 +129,20 @@ else
     read -p "Remove the USB drive, take it to VxCertifier, and bring it back to this machine when prompted. Press enter once you've re-inserted the USB drive. "
 fi
 
+#
+# Copy cert(s) off of USB drive to appropriate locations
+#
+
+# Applies permissions to match the permissions of other non-executable files in VX_CONFIG_ROOT
+function match_vx_config_non_executable_file_permissions() {
+    local file_path="${1}"
+    chown vx-vendor:vx-group "${file_path}"
+    chmod u=rw,g=r,o= "${file_path}"
+}
+
 while true; do
-    "${VX_FUNCTIONS_ROOT}/select-usb-drive-and-mount.sh"
-    if [[ -f "${USB_DRIVE_CERTS_DIRECTORY}/cert.pem" ]]; then
+    select_usb_drive_and_mount
+    if [[ -f "${USB_DRIVE_CERT_PATH}" ]]; then
         break
     fi
     read -p "Cert not found on USB drive. Double check that you've inserted the right USB drive and given it time to mount. Press enter to try again. "
@@ -127,56 +150,69 @@ done
 echo "Cert found on USB drive!"
 
 echo "Copying cert to ${MACHINE_CERT_PATH}..."
-cp "${USB_DRIVE_CERTS_DIRECTORY}/cert.pem" "${MACHINE_CERT_PATH}"
+cp "${USB_DRIVE_CERT_PATH}" "${MACHINE_CERT_PATH}"
 match_vx_config_non_executable_file_permissions "${MACHINE_CERT_PATH}"
 
 if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-  echo "Copying strongswan cert to ${STRONGSWAN_X509_PATH}..."
-  cp "${USB_DRIVE_CERTS_DIRECTORY}/vx-poll-book-strongswan-cert.pem" "${STRONGSWAN_X509_PATH}"
-  cp "${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem" "${STRONGSWAN_CA_PATH}"
+  echo "Copying strongSwan cert to ${MACHINE_STRONGSWAN_CERT_PATH}..."
+  cp "${USB_DRIVE_STRONGSWAN_CERT_PATH}" "${MACHINE_STRONGSWAN_CERT_PATH}"
+  cp "${ROOT_VX_CERT_AUTHORITY_CERT_PATH}" /etc/swanctl/x509ca/vx-cert-authority-cert.pem
 fi
 
-rm -rf "${USB_DRIVE_CERTS_DIRECTORY}"
+clean_up_usb_drive
 unmount_usb_drive
 
-# Cert correctness check 1
-if ! openssl x509 -in "${MACHINE_CERT_PATH}" -noout -pubkey | \
-    diff -q "${VX_CONFIG_ROOT}/key.pub" -; then
-    echo -e "\e[31mPublic key in machine cert doesn't match public key extracted from TPM\e[0m" >&2
-    read -p "Press enter to start over. "
-    exit 1
-fi
 #
-# Cert correctness check 1 for pollbook
-if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-  if ! openssl x509 -in "${STRONGSWAN_X509_PATH}" -noout -pubkey | \
-      diff -q "${VX_CONFIG_ROOT}/vx-poll-book-strongswan-rsa-cert.pub" -; then
-      echo -e "\e[31mPublic key in pollbook cert doesn't match the public key created by the TPM\e[0m" >&2
-      read -p "Press enter to start over. "
-      exit 1
-  fi
-fi
+# Perform correctness checks
+#
 
-# Cert correctness check 2
-# Note: Setting an -attime in the future allows us to accept a cert that has a start time slightly
-# in the future because the clock on VxCertifier was slightly ahead of the machine clock
-if ! openssl verify \
-    -attime "$(date -d "+10 minutes" +%s)" \
-    -CAfile "${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem" "${MACHINE_CERT_PATH}" > /dev/null; then
-    echo -e "\e[31mMachine cert was not signed by the correct cert authority or is not yet valid because of a clock mismatch\e[0m" >&2
+function error_and_start_over() {
+    echo -e "\e[31m$1\e[0m" >&2
     read -p "Press enter to start over. "
     exit 1
-fi
+}
 
-# Cert correctness check 2 for pollbook
+function check_cert_contains_correct_public_key() {
+    local cert_path="$1"
+    local public_key_path="$2"
+    local cert_identifier="$3"
+
+    if ! openssl x509 -in "${cert_path}" -noout -pubkey | \
+        diff -q "${public_key_path}" - >/dev/null; then
+        error_and_start_over "Public key in ${cert_path} doesn't match public key extracted from TPM"
+    fi
+}
+
+function check_cert_signed_by_correct_cert_authority() {
+    local cert_path="$1"
+    local cert_authority_cert_path="$2"
+    local error_message="$3"
+
+    # Note: Setting an -attime in the future allows us to accept a cert that has a start time
+    # slightly in the future because the clock on VxCertifier was slightly ahead of the machine clock
+    if ! openssl verify \
+        -attime "$(date -d "+10 minutes" +%s)" \
+        -CAfile "${cert_authority_cert_path}" "${cert_path}" >/dev/null; then
+        error_and_start_over "${cert_path} was not signed by the correct cert authority or is not yet valid because of a clock mismatch"
+    fi
+}
+
+check_cert_contains_correct_public_key \
+    "${MACHINE_CERT_PATH}" \
+    "${VX_CONFIG_ROOT}/key.pub"
+
+check_cert_signed_by_correct_cert_authority \
+    "${MACHINE_CERT_PATH}" \
+    "${ROOT_VX_CERT_AUTHORITY_CERT_PATH}"
+
 if [[ "${VX_MACHINE_TYPE}" == "poll-book" ]]; then
-  if ! openssl verify \
-      -attime "$(date -d "+10 minutes" +%s)" \
-      -CAfile "${VX_METADATA_ROOT}/vxsuite/libs/auth/certs/prod/vx-cert-authority-cert.pem" "${STRONGSWAN_X509_PATH}" > /dev/null; then
-      echo -e "\e[31mPollbook cert was not signed by the correct cert authority or is not yet valid because of a clock mismatch\e[0m" >&2
-      read -p "Press enter to start over. "
-      exit 1
-  fi
+    check_cert_contains_correct_public_key \
+        "${MACHINE_STRONGSWAN_CERT_PATH}" \
+        "${VX_CONFIG_ROOT}/vx-poll-book-strongswan-rsa-cert.pub"
+
+    check_cert_signed_by_correct_cert_authority \
+        "${MACHINE_STRONGSWAN_CERT_PATH}" \
+        "${ROOT_VX_CERT_AUTHORITY_CERT_PATH}"
 fi
 
 echo "Machine cert(s) saved! You can remove the USB drive."
