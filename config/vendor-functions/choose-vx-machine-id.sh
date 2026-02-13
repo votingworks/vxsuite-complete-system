2b0e26e919e62a32f08e629df71442c4d5080eb1
Arsalan Sufi <arsalan@voting.works>
2026-02-13 15:48:30 -0500

    Make machine ID example more representative and further restrict format

--- config/vendor-functions/choose-vx-machine-id.sh
+++ config/vendor-functions/choose-vx-machine-id.sh
@@ -12,8 +12,8 @@ if [[ "${VX_MACHINE_ID}" != "0000" ]]; then
     echo "Current Machine ID: ${VX_MACHINE_ID}"
 fi
 while true; do
-  read -p "Enter Machine ID (e.g. 0012): " MACHINE_ID
-  if [[ "${MACHINE_ID}" =~ ^[-0-9A-Z]+$ ]]; then
+  read -p "Enter Machine ID (e.g. AB-01-001): " MACHINE_ID
+  if [[ "${MACHINE_ID}" =~ ^[A-Z]{2,}-[0-9]{2,}-[0-9]{3,}$ ]]; then
     read -p "Confirm that Machine ID should be set to: ${MACHINE_ID} (y/n) " CONFIRM
     if [[ "${CONFIRM}" = "y" ]]; then
       mkdir -p "${VX_CONFIG_ROOT}"
@@ -33,6 +33,6 @@ while true; do
       break
     fi
   else
-    echo -e "\e[31mExpected Machine ID to be non-empty and contain only numbers, uppercase letters, and dashes, got: ${MACHINE_ID}\e[0m" >&2
+    echo -e "\e[31mExpected Machine ID to match format: [2+ letters]-[2+ digits]-[3+ digits] (e.g. AB-01-001), got: ${MACHINE_ID}\e[0m" >&2
   fi
 done
