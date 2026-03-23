362560feb3adeaaba40afec4b72e5d0739ba7bde
Adam McManus <adam.mcmanus@gmail.com>
2026-03-23 10:22:27 -0500

    script to manage local ethernet service as vx-vendor

---
+++ config/vendor-functions/manage-local-ethernet.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# This script enables strict management of the systemd-networkd service
+# It only supports specific command line arguments rather than granting
+# less restrictive actions
+
+set -euo pipefail
+
+service_action=$1
+
+if [[ -z ${service_action} ]]; then
+  echo "Usage: $0 action"
+  echo "Valid actions: enable, disable"
+  exit 1
+fi
+
+if [[ ${service_action} == "enable" ]]; then
+  systemctl enable --now systemd-networkd
+elif [[ ${service_action} == "disable" ]]; then
+  systemctl disable --now systemd-networkd
+else
+  echo "Error: ${service_action} is not a valid option"
+  echo "Valid actions: enable, disable"
+  exit 1
+fi
+
+exit 0
