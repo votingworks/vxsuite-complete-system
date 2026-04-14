5fdfcb1eba769669182c3b116cb57becf9ac45da
Adam McManus <adam.mcmanus@gmail.com>
2026-04-14 09:48:14 -0500

    Support updated build partition layout (#509)
    * more build changes and cleanup
    
    * getting the order right to reclaim as much space as possible
    
    * no longer using var partition for code repo downloads

--- config/vx-cleanup.sh
+++ config/vx-cleanup.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+LVM_BUILD_MOUNT_PATH="/vxbuild"
+LVM_DEVICE_PATH="/dev/Vx-vg/vxbuild"
+
 # Various files and directories to clean up during
 # VM shutdown in the build process
-/usr/bin/find /home/vx -mindepth 1 -delete
+/usr/bin/find "$(realpath /home/vx)" -mindepth 1 -delete
 /usr/bin/rm -f /var/log/*.log
 /usr/bin/rm -f /var/log/syslog
 /usr/bin/rm -f /var/log/votingworks/*
+
+# fstrim the build volume. It must be mounted or space won't
+# be reclaimed
+# unmount the LVM build volume
+# Remove it from /etc/fstab
+# Remove the LVM volume so space can be used by /var later
+if mountpoint -q "${LVM_BUILD_MOUNT_PATH}"; then
+  fstrim "${LVM_BUILD_MOUNT_PATH}"
+  umount "${LVM_BUILD_MOUNT_PATH}"
+  sed -i -e /vxbuild/d /etc/fstab
+  if lvdisplay "${LVM_DEVICE_PATH}"; then
+    lvremove -f /dev/Vx-vg/vxbuild
+  fi
+fi
+
 /usr/bin/systemctl disable vx-cleanup.service
 
 exit 0
